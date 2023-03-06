e8e3d47bcd75a42d94953d3e8f56aa3c29737200
tmeunier <theo.meunier41@gmail.com>
2023-03-06 17:49:28 +0100

    first commit

---
+++ app/src/main/java/com/example/medidoc_bts_h3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.medidoc_bts_h3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
