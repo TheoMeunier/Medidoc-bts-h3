e374ec3a7a3a08754fd43f1d3a88d6e2dcd1c55f
tmeunier <theo.meunier41@gmail.com>
2023-03-17 10:16:36 +0100

    clear code

--- app/src/main/java/com/example/medidoc_bts_h3/MainActivity.java
+++ app/src/main/java/com/example/medidoc_bts_h3/MainActivity.java
@@ -5,13 +5,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import com.example.medidoc_bts_h3.databinding.ActivityMainBinding;
+import com.example.medidoc_bts_h3.fragment.HomeFragment;
+import com.example.medidoc_bts_h3.fragment.ProfileFragment;
+import com.example.medidoc_bts_h3.fragment.SettingsFragment;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
